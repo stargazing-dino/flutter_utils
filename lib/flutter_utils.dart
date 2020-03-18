a81cfcae4c744d2fed8d90c7ad10a62a602b3a4f
Katzen <katzen@Katzens-MacBook-Pro.local>
2020-03-18 17:10:01 -0600

    Added getTextColor

--- lib/flutter_utils.dart
+++ lib/flutter_utils.dart
@@ -1,3 +1,4 @@
 library flutter_utils;
 
 export 'package:flutter_utils/get_status_code_error.dart';
+export 'package:flutter_utils/get_text_color.dart';
---
+++ lib/get_text_color.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/rendering.dart';
+
+/// Returns either `white` or `black` based off [backgroundColor].
+///
+/// E.g. a dark [backgroundColor] will return a white.
+Color getTextColor(Color backgroundColor) {
+  if (backgroundColor == null) return null;
+
+  var d = 0;
+
+  // Counting the perceptive luminance - human eye favors green background color...
+  final luminance = (0.299 * backgroundColor.red +
+          0.587 * backgroundColor.green +
+          0.114 * backgroundColor.blue) /
+      255;
+
+  if (luminance > 0.5) {
+    d = 30; // bright colors - black font - // #686868
+  } else {
+    d = 255; // dark colors - white font
+  }
+
+  return Color.fromARGB(backgroundColor.alpha, d, d, d);
+}
