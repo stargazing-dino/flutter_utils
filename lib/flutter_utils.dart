309b1a5c0321c4a554c212297946b1d59534027d
Katzen <katzen@Katzens-MacBook-Pro.local>
2020-03-18 17:24:20 -0600

    Forgot to export said files

--- lib/flutter_utils.dart
+++ lib/flutter_utils.dart
@@ -1,6 +1,8 @@
 library flutter_utils;
 
 export 'src/constants.dart';
+export 'src/enum_utils.dart';
 export 'src/get_status_code_error.dart';
 export 'src/get_text_color.dart';
 export 'src/show_snack_bar.dart';
+export 'src/string_utils.dart';
