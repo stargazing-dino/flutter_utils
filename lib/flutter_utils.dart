f1044b8f755aba1fa9be2cc41fa828708318f4c8
Katzen <katzen@Katzens-MacBook-Pro.local>
2020-04-08 07:02:38 -0600

    forgot to export

--- lib/flutter_utils.dart
+++ lib/flutter_utils.dart
@@ -3,6 +3,7 @@ library flutter_utils;
 export 'src/constants.dart';
 export 'src/date_utils.dart';
 export 'src/enum_utils.dart';
+export 'src/firebase_converters.dart';
 export 'src/get_status_code_error.dart';
 export 'src/get_text_color.dart';
 export 'src/lifecycle_event_handler.dart';
