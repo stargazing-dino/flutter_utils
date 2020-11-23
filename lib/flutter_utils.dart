df5ccfe7f255156260ca1b17a6316b2d9da427bf
Nolence <rex.magana@gmail.com>
2020-11-23 13:56:28 -0700

    Removed firebase converters

--- lib/flutter_utils.dart
+++ lib/flutter_utils.dart
@@ -3,7 +3,6 @@ library flutter_utils;
 export 'src/constants.dart';
 export 'src/date_utils.dart';
 export 'src/enum_utils.dart';
-export 'src/firebase_converters.dart';
 export 'src/get_status_code_error.dart';
 export 'src/get_text_color.dart';
 export 'src/show_snack_bar.dart';
